5c8bf88a013d8dd4ed4df61392457b9c13a1dc45
bhartiujjawala88 <84626157+bhartiujjawala88@users.noreply.github.com>
2021-05-22 18:23:20 +0530

    Add files via upload

---
+++ Greater.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Greater
+{
+	public static void main(String args[])
+	{
+		Scanner obj= new Scanner(System.in);
+		     System.out.println("enter the two number ");
+			 int A = obj.nextInt();
+			 int B = obj.nextInt();
+			 
+	if(A>B)
+	{
+	System.out.println(A+" is greater than "+B);
+	}
+	else
+	{
+     System.out.println(B+" is greater than "+A);
+	}
+	}
+}	
+		
+
+		
