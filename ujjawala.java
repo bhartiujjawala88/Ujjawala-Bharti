2b67c92a0007cb31dc2b46837ab3e0cc931c4c13
bhartiujjawala88 <84626157+bhartiujjawala88@users.noreply.github.com>
2021-05-22 18:25:54 +0530

    Add files via upload

---
+++ ujjawala.java
@@ -0,0 +1,15 @@
+class ujjawala
+{
+	public static void main(String args[])
+	
+	{
+		if(5<9)
+		{
+		System.out.println("system1");
+		}
+		else
+		{
+		System.out.println("system2");	
+		}
+    }
+}
