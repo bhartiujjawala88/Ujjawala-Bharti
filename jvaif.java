49d9ed5e9691d006764219e10341141b29bbe697
bhartiujjawala88 <84626157+bhartiujjawala88@users.noreply.github.com>
2021-05-22 18:25:28 +0530

    Add files via upload

---
+++ jvaif.java
@@ -0,0 +1,15 @@
+class Ujjawala
+{
+	public static void main(String args[])
+	
+	{
+		if(5<9)
+		{
+		System.out.println("system1");
+		}
+		else
+		{
+		System.out.println("system2");	
+		}
+    }
+}
